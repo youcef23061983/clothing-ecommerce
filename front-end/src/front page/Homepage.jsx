9df052b57a4d8f489cf79671717e1ea0b686cc6a
youcef23061983 <youcef.belcourtois@gmail.com>
2024-04-18 07:51:28 +0100

    17th commit

--- front-end/src/front page/Homepage.jsx
+++ front-end/src/front page/Homepage.jsx
@@ -4,9 +4,9 @@ import Product from "../pages/Product";
 import UseFetch from "../data managment/UseFetch";
 import { motion, useScroll, useTransform } from "framer-motion";
 
-const Homepage = ({ key1 }) => {
+const Homepage = () => {
   const url = "http://localhost:3000/products";
-  const { data, isPending, error } = UseFetch(url, key1);
+  const { data, isPending, error } = UseFetch(url);
 
   const [user, setUser] = useState({
     type: "all",
