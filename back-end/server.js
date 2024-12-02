183e8281326904de920bef09d6ae7fdbddc94aac
youcef23061983 <youcef.belcourtois@gmail.com>
2024-12-02 12:17:35 +0100

    products commit

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("Products.json");
+const router = jsonServer.router("products.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
