383e206bfc4120bdb398ab2b1f8c78b2a32cefd2
youcef23061983 <youcef.belcourtois@gmail.com>
2025-02-02 19:46:06 +0100

    SEO12 commit

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,23 +1,8 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react-swc";
-
-// export default defineConfig({
-//   plugins: [react()],
-//   test: {
-//     // 👋 add the line below to add jsdom to vite
-//     environment: "jsdom",
-//     // hey! 👋 over here
-//     globals: true,
-//     setupFiles: "./src/tests/SetupTest.jsx",
-//   },
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import ssr from "vite-plugin-ssr/plugin";
 
 export default defineConfig({
-  plugins: [react(), ssr()],
+  plugins: [react()],
   test: {
     // 👋 add the line below to add jsdom to vite
     environment: "jsdom",
