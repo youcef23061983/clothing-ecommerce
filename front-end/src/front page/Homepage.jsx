ce22c405c53dbaa4efc298a490b787de7607e753
youcef23061983 <youcef.belcourtois@gmail.com>
2025-02-06 15:44:57 +0100

    json commit

--- front-end/src/front page/Homepage.jsx
+++ front-end/src/front page/Homepage.jsx
@@ -5,36 +5,7 @@ import { motion } from "framer-motion";
 import { useSearchParams } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
 import { ReactLenis, useLenis } from "@studio-freight/react-lenis";
-const useMediaQuery = (query) => {
-  const [matches, setMatches] = useState(false);
 
-  useEffect(() => {
-    const media = window.matchMedia(query);
-    if (media.matches !== matches) {
-      setMatches(media.matches);
-    }
-
-    const listener = () => {
-      setMatches(media.matches);
-    };
-
-    if (typeof media.addEventListener === "function") {
-      media.addEventListener("change", listener);
-    } else {
-      media.addListener(listener);
-    }
-
-    return () => {
-      if (typeof media.removeEventListener === "function") {
-        media.removeEventListener("change", listener);
-      } else {
-        media.removeListener(listener);
-      }
-    };
-  }, [query]);
-
-  return matches;
-};
 const HeavyComponent = lazy(() => import("./Products"));
 
 const Homepage = () => {
