63fdd5c08f3003946187d562d23c062ed1e753fd
youcef23061983 <youcef.belcourtois@gmail.com>
2025-05-26 19:48:07 +0100

    ser commit1

--- back-end/server.js
+++ back-end/server.js
@@ -21,41 +21,6 @@ app.use(morgan("dev"));
 app.use("/products", productsRoutes);
 app.use("/auth", authRoutes);
 
-// app.post("/checkout", async (req, res) => {
-//   const session = await stripe.checkout.sessions.create({
-//     line_items: [
-//       {
-//         price_data: {
-//           currency: "usd",
-//           product_data: {
-//             name: "Node.js and Express book",
-//           },
-//           unit_amount: 50 * 100,
-//         },
-//         quantity: 1,
-//       },
-//       {
-//         price_data: {
-//           currency: "usd",
-//           product_data: {
-//             name: "JavaScript T-Shirt",
-//           },
-//           unit_amount: 20 * 100,
-//         },
-//         quantity: 1,
-//       },
-//     ],
-//     payment_method_types: ["card"],
-//     mode: "payment",
-//     success_url: `http://localhost:3000/complete?session_id={CHECKOUT_SESSION_ID}`,
-//     cancel_url: "http://localhost:3000/cancel",
-//     shipping_address_collection: {
-//       allowed_countries: ["US", "CA", "FR", "DZ"],
-//     },
-//   });
-//   console.log(session);
-//   res.redirect(303, session.url);
-// });
 app.post("/create-payment-intent", async (req, res) => {
   const { total } = req.body;
   console.log("total", total);
