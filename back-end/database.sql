ade99d0732facc92307fd66f34bd0db0f531ae1e
youcef23061983 <youcef.belcourtois@gmail.com>
2025-11-16 16:33:18 +0100

    add ivoice & orderId commit

--- back-end/database.sql
+++ back-end/database.sql
@@ -376,6 +376,8 @@ CREATE TABLE orders (
 );
 ALTER TABLE orders 
 ADD COLUMN phone VARCHAR(16) CHECK (phone ~ '^\+[1-9]\d{1,14}$')
+ALTER TABLE orders ADD COLUMN invoice_url TEXT;
+ALTER TABLE orders ADD COLUMN orderId TEXT;
 
 CREATE TABLE order_items (
   id SERIAL PRIMARY KEY,
